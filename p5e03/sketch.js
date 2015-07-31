f4cc3fe4654c18870501bea3170e89af865dfc0d
retroglyph <spasos@gmail.com>
2015-07-31 09:49:12 -0700

    added a 3rd experiment, color cycling with a touch command to randomly change col and div

---
+++ p5e03/sketch.js
@@ -0,0 +1,25 @@
+var col = 0,
+    sp = 1,
+    div = 3;
+
+function setup() {
+    createCanvas(350, 640);
+    noStroke();
+}
+
+function draw() {
+    background(col+=sp, 255-col, 255-col/div);
+    if (col > 255 || col < 0) {
+        sp *= -1;
+    }
+    fill(255-col, col, col/div);
+    ellipse(175, 320, 175 + col/div, 320 + 76 - col/div);
+}
+
+function touchStarted()
+{
+  // create a random background color
+  col = random(255);
+  div = random(3) + 1.5;
+  console.log('div:' + div + ", " + "col:" + col);
+}
